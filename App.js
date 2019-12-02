b724c28a5a596efa871d12a7744226215309fca2
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-02 09:37:25 +0800

    google services

--- App.js
+++ App.js
@@ -1,6 +1,3 @@
-/* eslint-disable quotes */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
 import AppContainer from './src/navigation/navigator';
 
@@ -10,99 +7,101 @@ import * as Utils from './src/utils/Utils';
 import firebase from 'react-native-firebase';
 
 export default class App extends Component {
+  constructor(properties) {
+    super(properties);
+    this.state = {
+      isAppDataLoaded: false,
+    };
+  }
 
-	constructor(properties) {
-		super(properties);
-		this.state = {
-			isAppDataLoaded: false,
-		};
-	}
+  async componentDidMount() {
+    try {
+      await LocalStorage.loadAppData();
+      console.log('appdata loaded::::');
+      this.setState({
+        isAppDataLoaded: true,
+      });
+    } catch (error) {
+      console.log('Error while loading appdata');
+      console.log(error);
+    }
 
-	async componentDidMount() {
+    this.checkPermission();
+    this.createNotificationListeners();
+  }
 
-		try {
-			await LocalStorage.loadAppData();
-			console.log("appdata loaded::::");
-			this.setState({
-				isAppDataLoaded: true,
-			});
-		} catch(error) {
-			console.log("Error while loading appdata");
-			console.log(error);
-		}
+  componentWillUnmount() {
+    this.onTokenRefreshListener();
+    this.notificationListener();
+    this.notificationDisplayedListener();
+  }
 
-		this.checkPermission();
-		this.createNotificationListeners();
-	}
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
+  }
 
-	componentWillUnmount() {
-		this.onTokenRefreshListener();
-		this.notificationListener();
-		this.notificationDisplayedListener();
-	}
+  async getToken() {
+    let fcmToken = LocalStorage.getUserInfo().fcmToken;
 
-	async checkPermission() {
-		const enabled = await firebase.messaging().hasPermission();
-		if (enabled) {
-			this.getToken();
-		} else {
-			this.requestPermission();
-		}
-	}
+    if (fcmToken === null || fcmToken === '') {
+      fcmToken = await firebase.messaging().getToken();
+      console.log('New FCM Token::' + fcmToken);
+      if (fcmToken) {
+        LocalStorage.setUserInfoData(LocalStorage.KEY_FCM_TOKEN, fcmToken);
+      }
+    } else {
+      console.log('Already have fcm token:::');
+      console.log(fcmToken);
+    }
+  }
 
-	async getToken() {
-		let fcmToken = LocalStorage.getUserInfo().fcmToken;
-		
-		if (fcmToken === null || fcmToken === '') {
-			fcmToken = await firebase.messaging().getToken();
-			console.log("New FCM Token::" + fcmToken);
-			if (fcmToken) {
-				LocalStorage.setUserInfoData(LocalStorage.KEY_FCM_TOKEN, fcmToken);
-			}
-		} else {
-			console.log("Already have fcm token:::");
-			console.log(fcmToken);
-		}
-	}
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      this.getToken();
+    } catch (error) {
+      console.log('permission rejected');
+    }
+  }
 
-	async requestPermission() {
-		try {
-			await firebase.messaging().requestPermission();
-			this.getToken();
-		} catch (error) {
-			console.log('permission rejected');
-		}
-	}
+  async createNotificationListeners() {
+    this.onTokenRefreshListener = firebase
+      .messaging()
+      .onTokenRefresh(fcmToken => {
+        console.log('token generated ->', fcmToken);
+        if (fcmToken) {
+          LocalStorage.setUserInfoData(LocalStorage.KEY_FCM_TOKEN, fcmToken);
+        }
+      });
 
-	async createNotificationListeners() {
+    this.notificationListener = firebase
+      .notifications()
+      .onNotification(notification => {
+        const {title, body} = notification;
+        console.log('onNotification:::');
+        console.log(notification);
+        Utils.showAlert(title, body);
+      });
 
-		this.onTokenRefreshListener = firebase.messaging().onTokenRefresh(fcmToken => {
-			console.log('token generated ->', fcmToken);
-			if (fcmToken) {
-				LocalStorage.setUserInfoData(LocalStorage.KEY_FCM_TOKEN, fcmToken);
-			}
-		});
+    this.notificationDisplayedListener = firebase
+      .notifications()
+      .onNotificationDisplayed(notification => {
+        const {title, body} = notification.notification;
+        console.log('onNotificationDisplayed:::');
+        console.log(notificationOpen.notification);
+        Utils.showAlert(title, body);
+      });
+  }
 
-		this.notificationListener = firebase.notifications().onNotification((notification) => {
-			const { title, body } = notification;
-			console.log("onNotification:::");
-			console.log(notification);
-			Utils.showAlert(title, body);
-		});
-	  
-		this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification) => {
-			const { title, body } = notification.notification;
-			console.log("onNotificationDisplayed:::");
-			console.log(notificationOpen.notification);
-			Utils.showAlert(title, body);
-        });
-		
-	}
-
-	render() {
-		if(this.state.isAppDataLoaded === false) {
-			return null;
-		}
-		return <AppContainer />;
-	}
+  render() {
+    if (this.state.isAppDataLoaded === false) {
+      return null;
+    }
+    return <AppContainer />;
+  }
 }
